838636165af0e75a1986037cfd7323160c23aed7
archnayadav204 <136904313+archnayadav204@users.noreply.github.com>
2024-01-26 00:40:20 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Archna </div>
+          <div className="right">
+            <p>HOTEL PALM PACIFIC, PANNA</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By ARCHNA YADAV</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By CodeWithLove.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
